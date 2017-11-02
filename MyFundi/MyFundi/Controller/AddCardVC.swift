b9352c33892ff825ad5d703810312d7aca90a020
Joseph Ishak <jishak1@asu.edu>
2017-11-01 20:44:27 -0700

    Completed the add card VC

--- MyFundi/MyFundi/Controller/AddCardVC.swift
+++ MyFundi/MyFundi/Controller/AddCardVC.swift
@@ -14,15 +14,15 @@ class AddCardVC: UIViewController, UITableViewDelegate , UITableViewDataSource{
     
     @IBOutlet weak var tableView: UITableView!
     
-    @IBOutlet weak var firstNameTextBox: UITextField!
+    @IBOutlet weak var firstNameTextBox: FancyField!
     
-    @IBOutlet weak var cardNumberTextBox: UITextField!
+    @IBOutlet weak var cardNumberTextBox: FancyField!
     
-    @IBOutlet weak var expireDateTextBox: UITextField!
+    @IBOutlet weak var expireDateTextBox: FancyField!
     
-    @IBOutlet weak var cvvTextBox: UITextField!
+    @IBOutlet weak var cvvTextBox: FancyField!
     
-    @IBOutlet weak var zipTextBox: UITextField!
+    @IBOutlet weak var zipTextBox: FancyField!
     
     
     var cards = [Card]()
@@ -31,27 +31,69 @@ class AddCardVC: UIViewController, UITableViewDelegate , UITableViewDataSource{
     var userRef: DatabaseReference!
     var methodKeys = [String]()
     var count: Int = 0
+    
     @IBAction func submitPressed(_ sender: Any) {
+        if validateValues() {
+            let card: Dictionary<String, AnyObject> = [
+                "cardName": firstNameTextBox.text as AnyObject,
+                "cardNumber": (cardNumberTextBox.text as! NSString).longLongValue as AnyObject,
+                "billingZip": (zipTextBox.text as! NSString).longLongValue as AnyObject,
+                "cvv": (cvvTextBox.text as! NSString).intValue as AnyObject,
+                "expirationDate": expireDateTextBox.text as AnyObject
+                
+            ]
+            
+            let firebaseCard = DataService.ds.REF_CARDS.childByAutoId()
+            var cardKey = firebaseCard.key
+            firebaseCard.setValue(card)
+            
+            firstNameTextBox.text = ""
+            cardNumberTextBox.text = ""
+            zipTextBox.text = ""
+            cvvTextBox.text = ""
+            expireDateTextBox.text = ""
+            UpdateFireBaseUser(cardKey: cardKey)
+        }else {
+            let alertController = UIAlertController(title: "Payment Method Error", message: "Please enter the fields noted", preferredStyle: UIAlertControllerStyle.alert)
+            alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default,handler: nil))
+            
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
+    
+    func UpdateFireBaseUser(cardKey: String){
+        
+        print("JOE!: \(cardKey)")
+        print("JOE!: \(self.userID)")
+    DataService.ds.REF_USERS.child(self.userID).child("paymentMethods").child(cardKey).setValue(true)
+        
+        viewDidLoad()
+        
     }
     @IBAction func numberPressed(_ sender: Any) {
-        if count == 1{
+        let chars = Array(expireDateTextBox.text!)
+        if chars.count == 2 {
             expireDateTextBox.text?.append("/")
+        }else  if chars.count >= 4{
+            expireDateTextBox.text?.append("")
         }
-        count = count + 1
+    
+      
+        
     }
-    @IBAction func numberEntered(_ sender: Any) {
-        if count == 1{
-            expireDateTextBox.text?.append("/")
-        }
-        count = count + 1
+    @IBAction func finishedEditing(_ sender: Any) {
+        count = 0
+        
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-        userID = (Auth.auth().currentUser?.uid)!
+        self.userID = (Auth.auth().currentUser?.uid)!
+        print("JOE: \(userID)")
         userRef = DataService.ds.REF_USERS.child(self.userID)
 
+        methodKeys = [String]()
         userRef.observeSingleEvent(of: .value, with: { (snapshot) in
             if let userDict = snapshot.value as? Dictionary<String,AnyObject> {
                 print ("JOE: USER DICT \(userDict)")
@@ -74,6 +116,50 @@ class AddCardVC: UIViewController, UITableViewDelegate , UITableViewDataSource{
         
         
         
+    }
+    func validateValues() ->Bool {
+        var nameIsValid :Bool = false
+         var dateIsValid :Bool = false
+         var numberIsValid :Bool = false
+         var cvvIsValid :Bool = false
+         var zipIsValid :Bool = false
+        
+        let cardArr = Array(cardNumberTextBox.text!)
+        let dateArr = Array(expireDateTextBox.text!)
+        
+        if firstNameTextBox.text == "" {
+            firstNameTextBox.errorBorder()
+             nameIsValid = false
+        } else{
+            nameIsValid = true
+            firstNameTextBox.normalBorder()
+        }
+        if cardNumberTextBox.text == "" || cardArr.count != 16  {
+            numberIsValid = false
+            cardNumberTextBox.errorBorder()
+        }else{
+            numberIsValid = true
+            cardNumberTextBox.normalBorder()
+        }
+        if expireDateTextBox.text ==  "" || dateArr.count != 5{
+            dateIsValid = false
+            expireDateTextBox.errorBorder()
+        } else {
+            dateIsValid = true
+            expireDateTextBox.normalBorder()
+        }
+        if zipTextBox.text == "" {
+            zipIsValid = false
+            zipTextBox.errorBorder()
+        } else {
+            zipIsValid = true
+            zipTextBox.normalBorder()
+        }
+        if zipIsValid, dateIsValid, numberIsValid, nameIsValid {
+            return true
+        } else {
+            return false
+        }
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let card = cards[indexPath.row]
@@ -102,9 +188,9 @@ class AddCardVC: UIViewController, UITableViewDelegate , UITableViewDataSource{
     }
     
     func loadPaymentMethods(){
-        self.cards = [Card]()
+       
         DataService.ds.REF_CARDS.observe(.value, with: { (snapshot) in
-            
+            var cards = [Card]()
             if let snapshot = snapshot.children.allObjects as? [DataSnapshot] {
                 for snap in snapshot {
                     print("JOE: \(snap.value)")
@@ -113,13 +199,14 @@ class AddCardVC: UIViewController, UITableViewDelegate , UITableViewDataSource{
                             if let cardDict = snap.value as? Dictionary<String, AnyObject> {
                                 let key = snap.key
                                 let  card = Card(cardKey: key, cardData: cardDict)
-                                self.cards.append(card)
+                               cards.append(card)
                                 
                             }
                         }
                     }
                 }
             }
+            self.cards = cards
             self.tableView.reloadData()
         })
         
