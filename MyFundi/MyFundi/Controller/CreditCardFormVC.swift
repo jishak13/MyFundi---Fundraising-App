262e930ec95750ca2dae36c0c3ef424b2e7530c3
kalibrahim <bowaleed500@gmail.com>
2017-11-07 19:15:24 -0700

    first commit

--- MyFundi/MyFundi/Controller/CreditCardFormVC.swift
+++ MyFundi/MyFundi/Controller/CreditCardFormVC.swift
@@ -10,13 +10,24 @@ import UIKit
 import Stripe
 import CreditCardForm
 import Firebase
-
 class CreditCardFormVC: UIViewController, STPPaymentCardTextFieldDelegate {
     
     @IBOutlet weak var creditCardForm: CreditCardFormView!
     
+    @IBOutlet weak var firstNameTextField: FancyField!
+    
+    @IBOutlet weak var zipTextField: FancyField!
     
     let paymentTextField = STPPaymentCardTextField()
+    
+    var cardNumber: String = ""
+    var expireDate: String = ""
+    var cvv: String = ""
+    var name: String = ""
+    var zip: String = ""
+    var userID: String = ""
+    var user: User!
+    var userRef: DatabaseReference!
     
  
     override func viewDidLoad() {
@@ -40,20 +51,35 @@ class CreditCardFormVC: UIViewController, STPPaymentCardTextFieldDelegate {
         view.addSubview(paymentTextField)
         
         NSLayoutConstraint.activate([
-            paymentTextField.topAnchor.constraint(equalTo: creditCardForm.bottomAnchor, constant: 20),
+            paymentTextField.topAnchor.constraint(equalTo: creditCardForm.bottomAnchor, constant: 50),
             paymentTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             paymentTextField.widthAnchor.constraint(equalToConstant: self.view.frame.size.width-20),
             paymentTextField.heightAnchor.constraint(equalToConstant: 44)
             ])
+        
+        self.userID = (Auth.auth().currentUser?.uid)!
+        print("JOE: \(userID)")
+        userRef = DataService.ds.REF_USERS.child(self.userID)
         // Do any additional setup after loading the view.
     }
     
     func paymentCardTextFieldDidChange(_ textField: STPPaymentCardTextField) {
         creditCardForm.paymentCardTextFieldDidChange(cardNumber: textField.cardNumber, expirationYear: textField.expirationYear, expirationMonth: textField.expirationMonth, cvc: textField.cvc)
+        
+        cardNumber = (textField.cardNumber as! String)
+        
+//
+//
+//        cvv = (textField.cvc as! String)
+        
+        print("CARD NUMBER:\(cardNumber) DATE: \(expireDate) CVC:\(cvv)")
+        
     }
     
     func paymentCardTextFieldDidEndEditingExpiration(_ textField: STPPaymentCardTextField) {
         creditCardForm.paymentCardTextFieldDidEndEditingExpiration(expirationYear: textField.expirationYear)
+        
+        expireDate = "\(textField.expirationMonth)/\(textField.expirationYear)"
     }
     
     func paymentCardTextFieldDidBeginEditingCVC(_ textField: STPPaymentCardTextField) {
@@ -62,9 +88,109 @@ class CreditCardFormVC: UIViewController, STPPaymentCardTextFieldDelegate {
     
     func paymentCardTextFieldDidEndEditingCVC(_ textField: STPPaymentCardTextField) {
         creditCardForm.paymentCardTextFieldDidEndEditingCVC()
+        cvv = textField.cvc as! String
+        print("CARD NUMBER:\(cardNumber) DATE: \(expireDate) CVC:\(cvv)")
     }
     
-
+    func validateValues() ->Bool {
+       
+        var nameIsValid :Bool = false
+        var dateIsValid :Bool = false
+        var numberIsValid :Bool = false
+        var cvvIsValid :Bool = false
+        var zipIsValid :Bool = false
+        
+        let cardArr = Array(cardNumber)
+        let dateArr = Array(expireDate)
+        
+        if firstNameTextField.text == "" {
+            firstNameTextField.errorBorder()
+            nameIsValid = false
+        } else{
+            nameIsValid = true
+            firstNameTextField.normalBorder()
+            if let theName = firstNameTextField.text {
+                name = theName
+            }
+        }
+        if cardNumber == "" || cardArr.count != 16  {
+            numberIsValid = false
+          
+        }else{
+            numberIsValid = true
+        
+        }
+        if expireDate ==  "" {
+            dateIsValid = false
+        
+        } else {
+            dateIsValid = true
+          
+        }
+        if zipTextField.text == "" {
+            zipIsValid = false
+            zipTextField.errorBorder()
+            
+        } else {
+            zipIsValid = true
+            zipTextField.normalBorder()
+            if let theZip = zipTextField.text {
+                zip = theZip
+            }
+        }
+        if cvv == "" {
+            cvvIsValid = false
+        
+        } else {
+            cvvIsValid = true
+     
+        }
+        if zipIsValid, dateIsValid, numberIsValid, nameIsValid,cvvIsValid {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    @IBAction func backPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func submitPressed(_ sender: Any) {
+        if validateValues() {
+            let card: Dictionary<String, AnyObject> = [
+                "cardName": name as AnyObject,
+                "cardNumber": (cardNumber as! NSString).longLongValue as AnyObject,
+                "billingZip": (zip as! NSString).longLongValue as AnyObject,
+                "cvv": (cvv as! NSString).intValue as AnyObject,
+                "expirationDate": expireDate as AnyObject
+                
+            ]
+            
+            let firebaseCard = DataService.ds.REF_CARDS.childByAutoId()
+            var cardKey = firebaseCard.key
+            firebaseCard.setValue(card)
+            
+            firstNameTextField.text = ""
+            zipTextField.text = ""
+           
+            UpdateFireBaseUser(cardKey: cardKey)
+        }else {
+            let alertController = UIAlertController(title: "Payment Method Error", message: "Please enter field correctly", preferredStyle: UIAlertControllerStyle.alert)
+            alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default,handler: nil))
+            
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
+    
+    func UpdateFireBaseUser(cardKey: String){
+        
+        print("JOE!: \(cardKey)")
+        print("JOE!: \(self.userID)")
+        DataService.ds.REF_USERS.child(self.userID).child("paymentMethods").child(cardKey).setValue(true)
+        
+        viewDidLoad()
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
